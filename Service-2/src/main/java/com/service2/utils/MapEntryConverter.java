f81c202e763af2052197d61199b56cf0ad424274
sagardas1 <sagardas.195@gmail.com>
2020-08-31 01:39:03 +0530

    chnages

--- Service-2/src/main/java/com/service2/utils/MapEntryConverter.java
+++ Service-2/src/main/java/com/service2/utils/MapEntryConverter.java
@@ -12,42 +12,40 @@ import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
 public class MapEntryConverter implements Converter {
 
-    public boolean canConvert(Class clazz) {
-        return AbstractMap.class.isAssignableFrom(clazz);
-    }
+	public boolean canConvert(Class clazz) {
+		return AbstractMap.class.isAssignableFrom(clazz);
+	}
 
-    public void marshal(Object value, HierarchicalStreamWriter writer, MarshallingContext context) {
+	public void marshal(Object value, HierarchicalStreamWriter writer, MarshallingContext context) {
 
-        AbstractMap map = (AbstractMap) value;
-        for (Object obj : map.entrySet()) {
-            Map.Entry entry = (Map.Entry) obj;
-            writer.startNode(entry.getKey().toString());
-            Object val = entry.getValue();
-            if ( null != val ) {
-                writer.setValue(val.toString());
-            }
-            writer.endNode();
-        }
+		AbstractMap map = (AbstractMap) value;
+		for (Object obj : map.entrySet()) {
+			Map.Entry entry = (Map.Entry) obj;
+			writer.startNode(entry.getKey().toString());
+			Object val = entry.getValue();
+			if (null != val) {
+				writer.setValue(val.toString());
+			}
+			writer.endNode();
+		}
 
-    }
+	}
 
-    public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+	public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
 
-        Map<String, String> map = new HashMap<String, String>();
+		Map<String, String> map = new HashMap<String, String>();
 
-        while(reader.hasMoreChildren()) {
-            reader.moveDown();
+		while (reader.hasMoreChildren()) {
+			reader.moveDown();
 
-            String key = reader.getNodeName(); // nodeName aka element's name
-            String value = reader.getValue();
-            map.put(key, value);
+			String key = reader.getNodeName(); // nodeName aka element's name
+			String value = reader.getValue();
+			map.put(key, value);
 
-            reader.moveUp();
-        }
+			reader.moveUp();
+		}
 
-        return map;
-    }
-
-	
+		return map;
+	}
 
 }
