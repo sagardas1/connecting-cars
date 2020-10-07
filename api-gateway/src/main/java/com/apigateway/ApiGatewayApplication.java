747785d304cf1f6cfbb25df2be788728a5c0625b
sagardas1 <sagardas.195@gmail.com>
2020-10-07 22:28:44 +0530

    changes

--- api-gateway/src/main/java/com/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -5,14 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
