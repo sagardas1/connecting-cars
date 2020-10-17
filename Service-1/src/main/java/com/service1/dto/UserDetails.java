1a52a86e854e048c80b7908df8d8493e5f2a0319
sagardas1 <sagardas.195@gmail.com>
2020-10-17 20:52:07 +0530

    changes

--- Service-1/src/main/java/com/service1/dto/UserDetails.java
+++ Service-1/src/main/java/com/service1/dto/UserDetails.java
@@ -5,30 +5,37 @@ public class UserDetails {
 	private String dob;
 	private String salary;
 	private int age;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDob() {
 		return dob;
 	}
+
 	public void setDob(String dob) {
 		this.dob = dob;
 	}
+
 	public String getSalary() {
 		return salary;
 	}
+
 	public void setSalary(String salary) {
 		this.salary = salary;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
 
 }
